eb22bd10d1f845eeda8dfe25efc1312a76ca6d41
Lucas Simon <lucassrod@gmail.com>
2014-08-29 20:52:17 -0300

    Cria as rotas para a api versao 1 de contatos

---
+++ apps/rest-node/routes/contacts.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.route('/v1/contatos')
+	.get(function(req, res) {
+		res.json({title:'Contatos - GET'});
+	})
+	.post(function(req, res) {
+		res.json({title:'Contatos - POST'});
+	});
+
+module.exports = router;
